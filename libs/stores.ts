5d46fd89cf3db39ab395f95ef8f55c60cd4cd73a
Ivcota <ivcotad@gmail.com>
2022-02-07 13:10:20 -0700

    authentication flow

--- libs/stores.ts
+++ libs/stores.ts
@@ -18,3 +18,32 @@ export const useNavStore = create<INavStore>((set) => ({
     });
   },
 }));
+
+interface UserState {
+  id: string;
+  name: string;
+  email: string;
+  photo: string;
+  isAdmin: boolean;
+}
+
+interface UserStateStore extends UserState {
+  setUserState: (UserState: UserState) => void;
+  setAdmin: (state: boolean) => void;
+}
+
+export const useUserStore = create<UserStateStore>((set) => {
+  return {
+    id: "",
+    name: "",
+    email: "",
+    photo: "",
+    isAdmin: false,
+    setUserState: ({ id, email, name, photo, isAdmin }: UserState) => {
+      set({ id, email, name, photo, isAdmin });
+    },
+    setAdmin: (state) => {
+      set({ isAdmin: state });
+    },
+  };
+});
